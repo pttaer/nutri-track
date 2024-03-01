95d98d4fd47170b2e862a18eebd7fbfad29b4ea4
misuta <sihoang36@gmail.com>
2024-03-01 16:51:49 +0700

    temp 2

--- Assets/2.Scripts/3.View/NTTStatisticView/NTTStatisticView.cs
+++ Assets/2.Scripts/3.View/NTTStatisticView/NTTStatisticView.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class NTTStatisticView : MonoBehaviour
 {
     private NTTBarChartView m_BarChartView;
+    private ToggleGroup m_ToggleGroup;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,15 @@ public class NTTStatisticView : MonoBehaviour
     public void Init()
     {
         m_BarChartView = transform.Find("Body/Viewport/Content/PnlChart").GetComponent<NTTBarChartView>();
+        m_ToggleGroup = transform.Find("Body/Viewport/Content/PnlChart/TglGr").GetComponent<ToggleGroup>();
+
+        foreach (Transform item in m_ToggleGroup.transform)
+        {
+            if (item.TryGetComponent(out NTTToggleFilterView tgl))
+            {
+                tgl.Init();   
+            }
+        }
 
         m_BarChartView.Init();
     }
